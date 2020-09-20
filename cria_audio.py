c24cb8bf690e47238ddd6b4f677dd2c37d238ab6
andersonwagner06 <andersonwa06@gmail.com>
2020-09-20 12:59:12 -0300

    Atualização do arquivo cria_audio
    Atualização do arquivo cria_audio

--- cria_audio.py
+++ cria_audio.py
@@ -1,4 +1,10 @@
 from gtts import gTTS
+from playsound import playsound
 
-tts = gTTS('Oi, eu sou a Rose', lang='pt-br')
-tts.save('audios/hello.mp3')
+def cria_audio(audio):
+    tts = gTTS(audio, lang='pt-br')
+    tts.save('audios/bem_vindo.mp3')
+
+    playsound('audios/bem_vindo.mp3')
+
+cria_audio('Oi, eu sou a Rose')
